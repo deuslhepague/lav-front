b02394dcb481d05c9dd81dc4d51d2846bddd375f
Mateus <mnslmateus@gmail.com>
2026-03-16 20:32:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Mude "lavanderia-app" para o nome do seu repositório no GitHub
-  base: '/lavanderia-app/',
+  base: '/lav-front/',
 })
